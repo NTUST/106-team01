a74842391aee230e71e5f04dcae31faf2dd89767
HueiYou <z851772198@gmail.com>
2017-06-07 07:46:02 -0500

    Add files via upload

--- ranking/classify/urls.py
+++ ranking/classify/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'classify'
 urlpatterns = [
     url(r'^$', views.multi2, name='multi'),
-    url(r'^hot/$', views.tea, name='tea'),
+    url(r'^hot/$', views.hot, name='hot'),
     url(r'^tea/$', views.tea, name='tea'),
+    url(r'^chew/$', views.chew, name='chew'),
     #url(r'^(?P<question_id>[0-9]+)/ans/$', views.tea, name='tea'),
 ]
