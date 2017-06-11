399261324e401fa467706aa84633b89d09043e67
b10430110 <dc0756sharon@gmail.com>
2017-06-11 19:35:13 +0800

    page

---
+++ LYS/ranking/classify/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'classify'
+urlpatterns = [
+    url(r'^$', views.multi2, name='multi'),
+    url(r'^hot/$', views.hot, name='hot'),
+    url(r'^tea/$', views.tea, name='tea'),
+    url(r'^chew/$', views.chew, name='chew'),
+    #url(r'^(?P<question_id>[0-9]+)/ans/$', views.tea, name='tea'),
+]
---
+++ LYS/ranking/polls/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.index, name='new'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^drinkbar2/$', views.drinkbar2, name='drinkbar2'),
+    url(r'^drinkbarcolor/$', views.drinkbarcolor, name='drinkbarcolor'),
+    url(r'^find/$', views.find, name='find'),
+    url(r'^store1/$', views.store1, name='store1'),
+    url(r'^store2/$', views.store2, name='store2'),
+    url(r'^store3/$', views.store3, name='store3'),
+    url(r'^store4/$', views.store4, name='store4'),
+    url(r'^store5/$', views.store5, name='store5'),
+    url(r'^store6/$', views.store6, name='store6'),
+    url(r'^store7/$', views.store7, name='store7'),
+    url(r'^store8/$', views.store8, name='store8'),
+    url(r'^store9/$', views.store9, name='store9'),
+    url(r'^store10/$', views.store10, name='store10'),
+    url(r'^(?P<question_id>[0-9]+)/information/$', views.information, name='information'),
+	url(r'^multi/$', views.multi, name='multi'),
+    # url(r'^tea/$', views.tea, name='tea'),
+]
+
