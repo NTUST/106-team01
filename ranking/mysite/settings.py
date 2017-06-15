967f6972dc1406ef68270a24b19db448fc4daa43
YI-HAN CHEN <hannah51125@gmail.com>
2017-06-15 17:34:13 +0800

    project change

--- ranking/mysite/settings.py
+++ ranking/mysite/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'multiselectfield',
-    'import_export',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
