9385a189601e0e1bef774a739b1228dbdd5b8777
hannahchen <hannah51125@gmail.com>
2018-04-12 22:08:56 +0800

    clean

--- LYS/ranking/classify/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from polls.models import Choice, Question, Store, Cate
-# Register your models here.
--- LYS/ranking/classify/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-from polls.models import Store, Drink
-# Create your models here.
